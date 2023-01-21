7de785c8f2fbd953a2a4df3aebf60b836067b684
Dist bit <distbit@pm.me>
2023-01-21 18:26:05 +1100

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-
-#export SNDCPY_APK="/home/pimania/uninstallablePrograms/sndcpy/sndcpy.apk"
-#/usr/bin/expect -f /home/pimania/uninstallablePrograms/sndcpy/script.exp &
 adb connect 192.168.0.243:5555
-scrcpy --tcpip --stay-awake --shortcut-mod lctrl --window-width 1920 --window-height 1080 --window-x=0 --window-y=0
+#scrcpy --tcpip --stay-awake --shortcut-mod lctrl --window-width 1920 --window-height 1080 --window-x=0 --window-y=0
 scrcpy --select-tcpip --stay-awake --shortcut-mod lctrl --window-width 1920 --window-height 1080 --window-x=0 --window-y=0
 
 #if the above doesn't work, try:

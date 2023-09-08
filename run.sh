da3ce5afc687981ac68bc784b93a4ecd640b109c
Dist Bit <adp01@pm.me>
2023-09-08 15:39:26 +1000

    final

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-adb connect 192.168.0.243:5555
+#adb connect 192.168.1.157:5555
+adb connect 192.168.0.13:5555
 #scrcpy --tcpip --stay-awake --shortcut-mod lctrl --window-width 1920 --window-height 1080 --window-x=0 --window-y=0
 scrcpy --select-tcpip --stay-awake --shortcut-mod lctrl --window-width 1920 --window-height 1080 --window-x=0 --window-y=0
 
